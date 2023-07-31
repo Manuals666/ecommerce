b4bbf2fa9fe065d6e9498adf450a26cf50dfa9eb
Manuals666 <manuel.deluna.garcia@gmail.com>
2023-07-31 17:32:16 -0600

    Se agrega router, links al menú y a la configuración inicial de TailwindCss

---
+++ src/components/molecules/header/Logo.jsx
@@ -0,0 +1,7 @@
+export const Logo = () => {
+  return (
+    <div>
+        <img src="https://app.ed.team/images/logo/isotipo-color.svg" alt="Logo eCommerce" />
+    </div>
+  )
+}
---
+++ src/components/templates/App.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom'
+import MainHeader from "../organisms/MainHeader"
+
+
+function App() {
+  return (
+    <div>
+        <MainHeader />
+        <Outlet />
+    </div>
+  )
+}
+
+export default App
