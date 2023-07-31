8a5f7f3a61deafc8447e303c84a1f01460ed3631
Manuals666 <manuel.deluna.garcia@gmail.com>
2023-07-31 14:21:31 -0600

    e modifica mensaje para desarollo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { API_URL } from "./env"
+import { API_URL } from "../env"
 
 function App() {
 
   return (
     <>
      <h1>Hola {API_URL}</h1>
+     <p>Este es el sitio en desarollo</p>
     </>
   )
 }
